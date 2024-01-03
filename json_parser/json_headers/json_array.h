9ec5e8abd0a9c66ae88544f0d60a145a43414173
Sovann Dufresne <sovann.dufresne@epitech.eu>
2024-01-03 14:19:48 +0100

    Fixed jpar headers

--- json_parser/json_headers/json_array.h
+++ json_parser/json_headers/json_array.h
@@ -10,9 +10,9 @@
 
     #include "json_types.h"
 
-void destroy_json_array(linked_list_t *list);
-void display_json_array(linked_list_t *array, int tab);
-linked_list_t *extract_json_array(char *str);
+void destroy_json_array(struct linked_list *list);
+void display_json_array(struct linked_list *array, int tab);
+struct linked_list *extract_json_array(char *str);
 char *extract_json_array_str(char *str);
 
 #endif /* !JSON_ARRAY_H_ */
--- json_parser/json_headers/json_pair.h
+++ json_parser/json_headers/json_pair.h
@@ -16,6 +16,6 @@ Creates new empty json key/value pair.
 json_pair_t *new_json_pair(void);
 void destroy_json_pair(json_pair_t *pair);
 void display_json_pair(json_pair_t *pair, int tab);
-void display_json_pairs(linked_list_t *pairs, int tab);
+void display_json_pairs(struct linked_list *pairs, int tab);
 
 #endif /* !JSON_PAIR_H_ */
